ad1aa6e0f9614a8680ecfd2e14c16a00838a2b47
nathanplt <nathan.l.zhang@gmail.com>
2024-10-31 21:33:46 -0700

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,7 @@ cart.forEach((cartItem) => {
   });
 
   orderSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${currProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -98,5 +98,8 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const { productId } = link.dataset;
       removeFromCart(productId);
+      
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.remove();
     });
   });
